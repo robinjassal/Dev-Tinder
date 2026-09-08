a85d87fcf9befe943dcc9c96b09b1aeeaa17dd59
Robin <robinjassal601@gmail.com>
2026-09-08 17:34:11 +0530

    routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,18 @@ const app = express();
 // app.use("/", (req, res) => {
 //   res.send("Home");
 // });
+
+//this will only get request to /user
+app.get("/user", (req, res) => {
+  res.send("user1");
+});
+
+app.post("/user", (req, res) => {
+  console.log("saving data");
+  res.send("Data saved in db");
+});
+
+//this will match all the HTTP methods API calls to /test
 app.use("/test/22", (req, res) => {
   res.send("test22");
 });
@@ -14,6 +26,11 @@ app.use("/test", (req, res) => {
 
 app.use("/hello", (req, res) => {
   res.send("test helo");
+});
+app.get("/products/:abc", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
+  res.send({ firstName: "robin", lastname: "jassal" });
 });
 
 app.listen(5000, () => {
