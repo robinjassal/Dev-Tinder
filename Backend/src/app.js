11561b584075e4a3a3f3f40961e677ba8f2bbaf2
Robin <robinjassal601@gmail.com>
2026-09-09 10:30:34 +0530

    route handler basics

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,36 +2,26 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Home");
-// });
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route user!!");
+    next();
+    // res.send("Response!!");
+  },
+  (req, res, next) => {
+    console.log("handling the route user 2!!");
+    next();
+    // res.send("2nd response");
+  },
+  (req, res, next) => {
+    console.log("handling the route user 3!!");
+    next();
+    // res.send("3rd response");
+  },
+);
 
-//this will only get request to /user
-app.get("/user", (req, res) => {
-  res.send("user1");
-});
-
-app.post("/user", (req, res) => {
-  console.log("saving data");
-  res.send("Data saved in db");
-});
-
-//this will match all the HTTP methods API calls to /test
-app.use("/test/22", (req, res) => {
-  res.send("test22");
-});
-app.use("/test", (req, res) => {
-  res.send("test1");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("test helo");
-});
-app.get("/products/:abc", (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.send({ firstName: "robin", lastname: "jassal" });
-});
+// app.use("/route",rH,[rH1,rh2,rH2],rH3) perfectly valid
 
 app.listen(5000, () => {
   console.log("Server is listening on Port 5000");
