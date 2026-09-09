d68abda2a0a79e617011ae8ddb57ae1ef80c8c87
Robin <robinjassal601@gmail.com>
2026-09-09 11:46:13 +0530

    error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,41 +1,16 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middleware/auth");
 
 const app = express();
 
-//Middleware: handle auth middleware for all request
-app.use("/admin", adminAuth);
-app.use("/user", userAuth, (req, res) => {
-  res.send("User Authenticated");
+app.use("/getUserData", (req, res) => {
+  throw new Error("User not found");
+  res.send("User Data Sent");
 });
 
-app.get("/admin/getAllData", (req, res, next) => {
-  /*Check if the request is authourised
-  const token = "xayz";
-  const isAuthorized = token === "xyz";
-  //Logic of fetching all data
-  if (isAuthorized) {
-    res.send("All Data Sent");
-  } else {
-    res.status(401).send("unautthorized request");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
   }
-*/
-  res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res, next) => {
-  /*Check if the request is authourised
-  const token = "xayz";
-  const isAuthorized = token === "xyz";
-  //Logic of fetching all data
-  if (isAuthorized) {
-    res.send("All Data Sent");
-  } else {
-    res.status(401).send("unautthorized request");
-  }
-  */
-  //Logic of fetching all data
-  res.send("All Data Deleted");
 });
 
 app.listen(5000, () => {
