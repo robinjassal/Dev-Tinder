e9f5ec6473bea4bfcad2ede8df2f8fd935c40d8d
Robin <robinjassal601@gmail.com>
2026-09-10 11:05:40 +0530

    crud ops

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,6 +23,68 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//Get user by email
+
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: email });
+    res.send(user);
+    if (!user) {
+      res.status(404).send("user not found");
+    }
+    //this is give all user even same email
+    // const user = await User.find({ emailId: email });
+    // if (user.length === 0) {
+    //   res.status(404).send("user not found");
+    // }
+    // res.send(user);
+  } catch (e) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//Feed API - GET /feed - get all the users from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // both are same it shorthand
+    const user = await User.findByIdAndDelete(userId);
+    // const user = await User.findByIdAndDelete({_id:userId})
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("user deleted successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established");
