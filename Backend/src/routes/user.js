87d19898ab8739e759caf3c0304ef1f792d32dfe
Robin <robinjassal601@gmail.com>
2026-09-12 18:04:47 +0530

    feed api

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequests");
+const User = require("../models/User");
 const userRouter = express.Router();
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 //Get all the pending connection request for the loggedIn user
 
@@ -18,7 +21,7 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
       data: connectionRequests,
     });
   } catch (error) {
-    req.statusCode(400).send("Error: " + error.message);
+    res.status(400).send("Error: " + error.message);
   }
 });
 
@@ -53,7 +56,55 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       data: data,
     });
   } catch (error) {
-    req.statusCode(400).send("Error: " + error.message);
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    // user should not see his own Card and which already connected, also ignore card and already sended requests
+    const loggedInUser = req.user;
+
+    //Find all connection requests (sent +received)
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        { toUserId: loggedInUser._id },
+      ],
+    });
+    //   .populate("fromUserId", "firstName")
+    //   .populate("toUserId", "firstName");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+    res.send(users);
+    // res.json({
+    //   message: "Data fetched successfully",
+    //   data: connectionRequests,
+    // });
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
   }
 });
 
