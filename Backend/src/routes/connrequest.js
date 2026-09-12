c2ea2b285e8081defbd8504688c27a2c4ba84baa
Robin <robinjassal601@gmail.com>
2026-09-12 13:24:50 +0530

    connected receive request

--- Backend/src/routes/connrequest.js
+++ Backend/src/routes/connrequest.js
@@ -64,4 +64,47 @@ connectionReqRouter.post(
   },
 );
 
+connectionReqRouter.post(
+  "/request/receive/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const status = req.params.status;
+      const requestId = req.params.requestId;
+      //Validate the status
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "invalid status type: " + status,
+        });
+      }
+      //Akshay =>Elon
+      //Is toUser Login: Elon loggedIn User
+      //status: only be interested
+      //request Id should be valid
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "connection request not found" });
+      }
+
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+
+      res.json({ message: "Connection request " + status, data });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send("ERROR: " + err.message);
+    }
+  },
+);
+
 module.exports = { connectionReqRouter };
